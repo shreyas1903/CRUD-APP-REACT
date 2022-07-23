c43ff20ff85730dc905c46e5b2ac6562c0a47a34
shreyas patil <shreyaspatil1901@gmail.com>
2022-07-23 22:33:45 +0530

    server

--- CRUD APPLICATION/Servers/App.js
+++ CRUD APPLICATION/Servers/App.js
@@ -1,3 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+
+const port = 8003;
+app.listen(port, () => {
+  console.log(`server is started on port number ${port}`);
+});
